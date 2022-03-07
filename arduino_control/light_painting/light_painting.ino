c1dc700749ffee47307a1a78c915e62c241a6cab
Mohammad Khan <mohammadukhan135@gmail.com>
2022-03-07 10:50:31 -0500

    Fixed Robot motion- started ROS--> Arduino Coms
    - Robot motion is fixed. No tuple error
    - Motion is different than expected. wpose.position.x moves robot in the z-direction in RVIZ. Other motions are described in the comments in the robot_motion script
    
    - ROS to Arduino communication seems easy. We can write python scripts and have it communicate with Arduino directly using the link provided in the robot_motion script & here https://create.arduino.cc/projecthub/ansh2919/serial-communication-between-python-and-arduino-e7cce0
    - Also able to edit Arduino code if needed in VS code & install ROS Serial Library in Arduino IDE to run example ROS code. All can be seen in the relevant scripts (robot_motion & light_painting.io)

--- arduino_control/light_painting/light_painting.ino
+++ arduino_control/light_painting/light_painting.ino
@@ -1,10 +1,37 @@
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+#include <ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/UInt16.h>
+
+#define BUTTON 8
+#define LED 13
+
+ros::NodeHandle node_handle;
+
+std_msgs::String button_msg;
+std_msgs::UInt16 led_msg;
+
+void subscriberCallback(const std_msgs::UInt16& led_msg) {
+  if (led_msg.data  == 1) {
+    digitalWrite(LED, HIGH); 
+  } else {
+    digitalWrite(LED, LOW);
+  }
 }
 
-// the loop function runs over and over again forever
+
+ros::Publisher button_publisher("button_press", &button_msg);
+// publisher name: button_publisher
+// publishes button_msg to button_press topic
+ros::Subscriber<std_msgs::UInt16> led_subscriber("toggle_led", &subscriberCallback);
+
+
+//// the setup function runs once when you press reset or power the board
+void setup() {
+//  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+//
+/// the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
--- scripts/robot_motion.py
+++ scripts/robot_motion.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python3
 
+from curses import baudrate
 import rospy
 from robot_control import * 
 import copy
 from geometry_msgs.msg import Pose
 
+# For Arduino Communication:
+# https://create.arduino.cc/projecthub/ansh2919/serial-communication-between-python-and-arduino-e7cce0
+import serial
+arduino = serial.Serial(port='/dev/ttyACM0',baudrate=9600,timeout=0.1)
+import time
+
+# Custom Scripts
+import light_painting.io as arduino
+
+# def write_read(x):
+#     arduino.write(bytes(x, 'utf-8'))
+#     time.sleep(0.05)
+#     data = arduino.readline()
+#     return data
+# while True:
+#     num = input("Enter a number: ") # Taking input from user
+#     value = write_read(num)
+#     print(value) # printing the value
+
+
+
 def main():
     rospy.init_node('light_painting', anonymous=False)
+    rospy.loginfo(">> light_painting Node sucessfully created")
+
     rc= moveManipulator('mh5l')
     rc.set_vel(0.1)
     rc.set_accel(0.1)
@@ -14,22 +38,52 @@ def main():
     rc.goto_all_zeros()
 
     waypoints = []
-    scale = 1
+    scale =0.5
 
     wpose = rc.move_group.get_current_pose().pose
-    wpose.position.x += scale * 0.8 
+    wpose.position.x += scale * 0.1 
+    # wpose.position.y: twists the arm about the blue axis
+    # wpose.position.x: moves robot forward
+    # wpose.position.z: moves robot vertically up
     waypoints.append(wpose)
 
-    plan = rc.plan_cartesian_path(waypoints)
+    plan, fraction = rc.plan_cartesian_path(waypoints)
+    # Added fraction because of this Github issue: 
+    # https://github.com/ros-planning/moveit/issues/709
 
     input("Cartesian Plan: press <enter>")
-    # input("Cartesian Plan: press <enter>")
+    # for Python 2.7: raw_input("Cartesian Plan: press <enter>")
     rc.execute_plan(plan)
 
+ #### Arduino Communication Plan: ############
+    # Send I/O to turn on light on arduino
+    # publish to cpp node
+    # then in Arduino script, 
+    # subscribe to cpp node to turn on light
+    # hopefully this works??
+    # NVM: we can write arduino code using python
+    # https://create.arduino.cc/projecthub/ansh2919/serial-communication-between-python-and-arduino-e7cce0
+    
+
+
     input("All zeros: press <enter>")
-    # input("All zeros: press <enter>")
+    # for Python 2.7: raw_input("All zeros: press <enter>")
 
     rc.goto_all_zeros()
+
+'''
+Plan for integrating Arduino with robot: 
+- Send I/O to arduino board via line of code (instead of pushbutton) Send Digital High maybe?
+- This turns light on/off at certain points
+- Move robot as normal in script. 
+'''
+
+
+'''
+for Arduino Control:
+Install Rosserial Library into Arduino
+Rosserial Arduino Examples: https://github.com/ros-drivers/rosserial/tree/dd76994c67c5e4997ef64837c07afb4eb0d4df27/rosserial_arduino/src/ros_lib/examples
+'''
 
 if __name__ == '__main__':
     main()
