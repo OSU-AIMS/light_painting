49840ff7fc9ae3bf9fb9e9ee5c327a512a9bb888
LuisC18 <martinez.737@osu.edu>
2022-03-09 13:12:02 -0500

    computeCartesianPlan error
    -tried adding a series of cartesian movements 1 m wide
    -plan rarely reaches 100% of desired pose, returns 0% sometimes
    -tried lowering max_step

--- scripts/robot_motion.py
+++ scripts/robot_motion.py
@@ -24,14 +24,15 @@ def main():
     rc.goto_all_zeros() 
 
     waypoints = []
-    scale =0.5
+    scale = 1
 
     wpose = rc.move_group.get_current_pose().pose
-    wpose.position.x += scale * 0.1 
-    # Robot motions don't match RVIZ Axis for EEF TCP
-    # wpose.position.y: twists the arm about the blue axis
-    # wpose.position.x: moves robot forward
-    # wpose.position.z: moves robot vertically up
+
+    # TOP LEFT (30cm up and 50 cm left from all zeros)
+    # Pose axis relative Robot origin axis
+    wpose.position.z += 0.3
+    wpose.position.y += -0.5
+    
     waypoints.append(wpose)
 
     plan, fraction = rc.plan_cartesian_path(waypoints)
@@ -42,6 +43,21 @@ def main():
     # for Python 2.7: raw_input("Cartesian Plan: press <enter>")
     rc.execute_plan(plan)
 
+
+    # Box 1m by 1m
+    MOTION_BOX_LENGTH = 1
+    # width is number of divisions over length
+    WIDTH = 10
+
+    for i in range(WIDTH):
+        waypoints = []
+        wpose.position.y += MOTION_BOX_LENGTH/WIDTH
+        waypoints.append(wpose)
+        rospy.loginfo(wpose)
+        plan, fraction = rc.plan_cartesian_path(waypoints)
+        input(f"Cartesian Plan {i}: press <enter>")
+        rc.execute_plan(plan)
+
     print(arduino.readline())             #read the serial data and print it as line
     print("Enter 1 to ON LED and 0 to OFF LED")
     user_input = input()                  #waits until user enters data
@@ -49,10 +65,8 @@ def main():
     arduino_led.control_led(user_input)     # call to turn LED on/off (as of 3/9: currently requires user input)
 
 
- #### Arduino Communication Plan: ############
+    #### Arduino Communication Plan: ############
     
-
-
     input("All zeros: press <enter>")
     # for Python 2.7: raw_input("All zeros: press <enter>")
 
