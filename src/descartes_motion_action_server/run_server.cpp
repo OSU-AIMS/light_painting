171bb99394ca0cfe96201d7c210a817581878dad
acbuynak <44533530+acbuynak@users.noreply.github.com>
2022-04-07 17:25:57 -0400

    Refactor:  Implement motion control as a Class (SimpleMover)
    * refactor Descartes commands into a class object
    * tested on simulation only

--- src/descartes_motion_action_server/run_server.cpp
+++ src/descartes_motion_action_server/run_server.cpp
@@ -27,18 +27,130 @@
 
 
 
-// ----------------------
-// Function Declarations
-// ----------------------
+// ------------------------
+// Descartes Wrapper Class
+// ------------------------
 
-// Build Path
-std::vector<descartes_core::TrajectoryPtPtr> makeStraightPath(
-    Eigen::Isometry3d pattern_start = Eigen::Isometry3d::Identity(),
-    Eigen::Isometry3d pattern_end = Eigen::Isometry3d::Identity()
+class SimpleMover {
+  public:
+
+    /* Public Variables */
+    const std::string world_frame = "base_link";
+    const std::string tcp_frame = "tool0";
+    ros::NodeHandle *nh;
+
+
+    /* Public Function Declarations */
+    bool executeTrajectory(const trajectory_msgs::JointTrajectory& trajectory);
+    
+
+    /* Constructor */
+    SimpleMover(ros::NodeHandle *nh_passthru, const std::string robot_group_name)
+    {  
+      nh = nh_passthru;
+
+      descartes_core::RobotModelPtr model_setup (new descartes_moveit::IkFastMoveitStateAdapter());
+      model = model_setup;
+
+      // Robot model description name on ROS Parameter Server
+      const std::string robot_description = "robot_description";
+
+      // Initialize model
+      if (!model->initialize(robot_description, robot_group_name, world_frame, tcp_frame))
+      {
+        ROS_INFO("Could not initialize robot model");
+      }
+      model->setCheckCollisions(true);
+
+      // Descartes: Setup DensePlanner
+      if (!planner.initialize(model))
+      {
+        ROS_ERROR("Failed to initialize planner");
+      }
+    }
+
+
+    /* New Move Request */
+    int MoveRequest(Eigen::Isometry3d pose_goal)
+    {
+      // Lookup current pose
+      Eigen::Isometry3d pattern_origin = CalcCurrentPose();
+
+      // Generate path plan starting from current pose
+      std::vector<descartes_core::TrajectoryPtPtr> points = makeStraightPath(pattern_origin, pose_goal);
+
+      // Descartes: Build Trajectory
+      // build large kinematic graph
+      if (!planner.planPath(points))
+      {
+        ROS_ERROR("Could not solve for a valid path");
+        return -3;
+      }
+      // search kinematic graph to build trajectory
+      std::vector<descartes_core::TrajectoryPtPtr> result;
+      if (!planner.getPath(result))
+      {
+        ROS_ERROR("Could not retrieve path");
+        return -4;
+      }
+
+      // Assemble ROS JointTrajectory Message
+      std::vector<std::string> names;
+      nh->getParam("controller_joint_names", names);
+
+      trajectory_msgs::JointTrajectory joint_solution;
+      joint_solution.joint_names = names;
+      
+      const static double default_joint_vel = 0.5;  // Unit:[rad/s]  Define a default velocity (ie. first point in path) 
+      if (!descartes_utilities::toRosJointPoints(*model, result, default_joint_vel, joint_solution.points))
+      {
+        ROS_ERROR("Unable to convert Descartes trajectory to joint points");
+        return -5;
+      }
+
+
+      // Send ROS trajectory to the robot action controller
+      if (!executeTrajectory(joint_solution))
+      {
+        ROS_ERROR("Could not execute trajectory!");
+        return -6;
+      }
+
+
+      // Wait till user kills the process (Control-C)
+      ROS_INFO("Done!");
+      return 0;
+    }
+
+
+    /* Get Current Pose */
+    Eigen::Isometry3d CalcCurrentPose(const std::string jointTopic = "/joint_states")
+    {
+      sensor_msgs::JointStateConstPtr current_state_ptr = ros::topic::waitForMessage<sensor_msgs::JointState>(jointTopic, ros::Duration(2));
+      Eigen::Isometry3d pattern_origin = Eigen::Isometry3d::Identity();
+      
+      model->getFK(current_state_ptr->position, pattern_origin);
+
+      return pattern_origin;
+    }
+
+
+  private:
+
+    /* Private Variables */
+    descartes_planner::DensePlanner planner;
+    descartes_core::RobotModelPtr model;
+
+    /* Private Function Declarations */
+    descartes_core::TrajectoryPtPtr makeCartesianPoint(const Eigen::Isometry3d& pose, double dt);
+    descartes_core::TrajectoryPtPtr makeTolerancedCartesianPoint(const Eigen::Isometry3d& pose, double dt);
+    std::vector<descartes_core::TrajectoryPtPtr> makeStraightPath(
+      Eigen::Isometry3d pattern_start = Eigen::Isometry3d::Identity(),
+      Eigen::Isometry3d pattern_end = Eigen::Isometry3d::Identity()
     );
+};
 
-// Send Motion Commands
-bool executeTrajectory(const trajectory_msgs::JointTrajectory& trajectory);
+
 
 
 
@@ -49,100 +161,29 @@ bool executeTrajectory(const trajectory_msgs::JointTrajectory& trajectory);
 int main(int argc, char** argv)
 {
   // Initialize ROS
-  ros::init(argc, argv, "single_descartes_move");
+  ros::init(argc, argv, "simple_mover_server");
   ros::NodeHandle nh;
 
   // Keep ROS Node & Supporting Processes Alive (single thread)
   ros::AsyncSpinner spinner (1);
   spinner.start();
 
-  // Robot model: kinematics, group name
-  descartes_core::RobotModelPtr model (new descartes_moveit::IkFastMoveitStateAdapter());
-  const std::string robot_description = "robot_description";
-  const std::string group_name = "mh5l";
-
-  // Reference frames for planning
-  const std::string world_frame = "base_link";
-  const std::string tcp_frame = "tool0";
-
-  // Initialize model
-  if (!model->initialize(robot_description, group_name, world_frame, tcp_frame))
-  {
-    ROS_INFO("Could not initialize robot model");
-    return -1;
-  }
-  model->setCheckCollisions(true);
-
-
-  // Get current pose
-  const std::string jointTopic = "/joint_states";
-  sensor_msgs::JointStateConstPtr current_state_ptr = ros::topic::waitForMessage<sensor_msgs::JointState>(jointTopic, ros::Duration(2));
-
-  Eigen::Isometry3d pattern_origin = Eigen::Isometry3d::Identity();
-  model->getFK(current_state_ptr->position, pattern_origin);
+  // Init SimpleMover Class
+  SimpleMover sm(&nh, "mh5l");
 
   // Get goal pose
   // TODO: get from action server
   // TEST ONLY: modify current position in one axis
-  Eigen::Isometry3d pattern_delta = Eigen::Isometry3d::Identity();
-  pattern_delta.translation() = Eigen::Vector3d(0, 0, -0.2);
+  Eigen::Isometry3d pattern_down = Eigen::Isometry3d::Identity();
 
-  Eigen::Isometry3d pose_goal = pattern_delta * pattern_origin;
+  pattern_down.translation()  = Eigen::Vector3d(0, 0, -0.2);
 
-  // Generate path plan starting from current pose
-  std::vector<descartes_core::TrajectoryPtPtr> points = makeStraightPath(pattern_origin, pose_goal);
+  Eigen::Isometry3d pattern_origin = sm.CalcCurrentPose();
 
+  Eigen::Isometry3d pose_down = pattern_down * pattern_origin;
 
-  // Descartes: Setup DensePlanner
-  descartes_planner::DensePlanner planner;
-  if (!planner.initialize(model))
-  {
-    ROS_ERROR("Failed to initialize planner");
-    return -2;
-  }
-
-
-  // Descartes: Build Trajectory
-  // build large kinematic graph
-  if (!planner.planPath(points))
-  {
-    ROS_ERROR("Could not solve for a valid path");
-    return -3;
-  }
-  // search kinematic graph to build trajectory
-  std::vector<descartes_core::TrajectoryPtPtr> result;
-  if (!planner.getPath(result))
-  {
-    ROS_ERROR("Could not retrieve path");
-    return -4;
-  }
-
-
-  // Assemble ROS JointTrajectory Message
-  std::vector<std::string> names;
-  nh.getParam("controller_joint_names", names);
-
-  trajectory_msgs::JointTrajectory joint_solution;
-  joint_solution.joint_names = names;
-  
-  const static double default_joint_vel = 0.5;  // Unit:[rad/s]  Define a default velocity (ie. first point in path) 
-  if (!descartes_utilities::toRosJointPoints(*model, result, default_joint_vel, joint_solution.points))
-  {
-    ROS_ERROR("Unable to convert Descartes trajectory to joint points");
-    return -5;
-  }
-
-
-  // Send ROS trajectory to the robot action controller
-  if (!executeTrajectory(joint_solution))
-  {
-    ROS_ERROR("Could not execute trajectory!");
-    return -6;
-  }
-
-  // Wait till user kills the process (Control-C)
-  ROS_INFO("Done!");
-  return 0;
+  // Call Move Request
+  sm.MoveRequest(pose_down);
 }
 
 
@@ -151,7 +192,7 @@ int main(int argc, char** argv)
 // Support Functions
 // ----------------------
 
-descartes_core::TrajectoryPtPtr makeCartesianPoint(const Eigen::Isometry3d& pose, double dt)
+descartes_core::TrajectoryPtPtr SimpleMover::makeCartesianPoint(const Eigen::Isometry3d& pose, double dt)
 {
   using namespace descartes_core;
   using namespace descartes_trajectory;
@@ -159,7 +200,7 @@ descartes_core::TrajectoryPtPtr makeCartesianPoint(const Eigen::Isometry3d& pose
   return TrajectoryPtPtr( new CartTrajectoryPt( TolerancedFrame(pose), TimingConstraint(dt)) );
 }
 
-descartes_core::TrajectoryPtPtr makeTolerancedCartesianPoint(const Eigen::Isometry3d& pose, double dt)
+descartes_core::TrajectoryPtPtr SimpleMover::makeTolerancedCartesianPoint(const Eigen::Isometry3d& pose, double dt)
 {
   using namespace descartes_core;
   using namespace descartes_trajectory;
@@ -172,7 +213,7 @@ descartes_core::TrajectoryPtPtr makeTolerancedCartesianPoint(const Eigen::Isomet
 // Generate Path
 // ----------------------
 
-std::vector<descartes_core::TrajectoryPtPtr> makeStraightPath(Eigen::Isometry3d pattern_start, Eigen::Isometry3d pattern_end)
+std::vector<descartes_core::TrajectoryPtPtr> SimpleMover::makeStraightPath(Eigen::Isometry3d pattern_start, Eigen::Isometry3d pattern_end)
 {
   // Extract Pose Position Start-End Differences
   Eigen::Isometry3d pattern_diff = Eigen::Isometry3d::Identity();
@@ -196,7 +237,7 @@ std::vector<descartes_core::TrajectoryPtPtr> makeStraightPath(Eigen::Isometry3d 
 
   // Ensure first trajectory point is at exact start
   std::vector<descartes_core::TrajectoryPtPtr> result;
-  descartes_core::TrajectoryPtPtr pt = makeCartesianPoint(pattern_start, 5);
+  descartes_core::TrajectoryPtPtr pt = makeCartesianPoint(pattern_start, 1);
   result.push_back(pt);
 
   // Assemble path as list of Descartes Points
@@ -216,17 +257,17 @@ std::vector<descartes_core::TrajectoryPtPtr> makeStraightPath(Eigen::Isometry3d 
 // Function Definitions
 // ----------------------
 
-bool executeTrajectory(const trajectory_msgs::JointTrajectory& trajectory)
+bool SimpleMover::executeTrajectory(const trajectory_msgs::JointTrajectory& trajectory)
 {
   // Create a Follow Joint Trajectory Action Client
   actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> ac ("joint_trajectory_action", true);
   if (!ac.waitForServer(ros::Duration(5.0)))
   {
-    ROS_ERROR("Could not connect to action server");
+    ROS_ERROR("SimpleMover: Could not connect to action server");
     return false;
   }
 
-  ROS_INFO("Connected to action server!");
+  ROS_INFO("SimpleMover: Connected to action server!");
 
   control_msgs::FollowJointTrajectoryGoal goal;
   goal.trajectory = trajectory;
