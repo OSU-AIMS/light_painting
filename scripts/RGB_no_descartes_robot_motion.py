6e8e7890359e653b3a880eb1f0b5ec7917ba654a
Mohammad Khan <mohammadukhan135@gmail.com>
2022-04-08 16:22:29 -0400

    Refactored RGB Script
    - Removed comments, notes, unused variables in RGB_no_descartes script
    - Added grayscale images for next feature

--- scripts/RGB_no_descartes_robot_motion.py
+++ scripts/RGB_no_descartes_robot_motion.py
@@ -22,17 +22,51 @@ from RGB_values_publisher import sendRGB2LED
 from light_painting.msg import RGBState
 
 
+#------------------------ Global Variables -----------------
+MOTION_BOX_scale = 0.01 # m 
+# import image
+img = input_image.RGB
+
+
+# Box length (m)
+IMAGE_HEIGHT = np.size(img,0) 
+print('height of image',IMAGE_HEIGHT)
+IMAGE_WIDTH = np.size(img,1)
+print('Width of image',IMAGE_WIDTH)
+
+# real world box size
+MOTION_BOX_WIDTH =  IMAGE_WIDTH*MOTION_BOX_scale # m
+MOTION_BOX_HEIGHT = IMAGE_HEIGHT*MOTION_BOX_scale # m
+
+# Starting positions for robot
+z_start = 1 # m
+y_start = -MOTION_BOX_WIDTH/2 # m
+#----------------------------------
+
+def nextRow(wpose):
+    wpose.position.z -= MOTION_BOX_scale
+    wpose.position.y = y_start # same y-axis starting value
+    waypoints= []
+    waypoints.append(copy.deepcopy(wpose))
+    return waypoints
+
+def nextColumn(wpose):
+    #increments to next column
+    wpose.position.y += MOTION_BOX_scale
+    waypoints= []
+    waypoints.append(copy.deepcopy(wpose))
+    return waypoints
+
+
 def main():
 
-    move_robot = True 
+    move_robot = True # set equal to False to not move robot
 
     # init node & PUblishers
     pub_rgb_values = rospy.Publisher('paintbrush_color',RGBState, queue_size=5)       
     rospy.init_node('picasso')
     rospy.loginfo(">>picasso node successfully created")
     rospy.Rate(1)
-
-    # Commented out below to the end to test image reading and LED
     
     rc= moveManipulator('mh5l')
     rc.set_vel(0.1)
@@ -42,92 +76,38 @@ def main():
         rc.goto_all_zeros() 
 
     waypoints = []
-    scale = 1
-
     start_pose = rc.move_group.get_current_pose().pose
-
-    # import image
-    rgb_img = input_image.RGB
-
-    # real world box size
-    MOTION_BOX_WIDTH =  .1 # m
-    MOTION_BOX_HEIGHT = .1
-
-    # Starting positions for robot
-    z_start = 1 # m
-    y_start = -MOTION_BOX_WIDTH/2 # m
-
     start_pose.position.z = z_start 
     start_pose.position.y = y_start
 
     if move_robot:
-        rc.goto_Pose(start_pose) # uncomment to remove robot motion
-    
-
-    # Box length (m)
-    IMAGE_HEIGHT = np.size(rgb_img,0) 
-    print('height of image',IMAGE_HEIGHT) # =3
-    # set this to length of input image = np.size(input_image.binary,0) = 3
-    IMAGE_WIDTH = np.size(rgb_img,1)
-     # set this to height of input image = 3
-    print('Width of image',IMAGE_WIDTH) #=3
-    PIXEL_COUNT = IMAGE_WIDTH*IMAGE_HEIGHT
-  
-
-    # if we want to have robot stop at sides of pixel rather than middle. Width = 4?
+        rc.goto_Pose(start_pose)
 
     row = range(IMAGE_HEIGHT) # [0,1,2]
     col = range(IMAGE_WIDTH) # [0,1,2]
+    
+    for i in row:
+        
+        if i  != 0: #
+            print('reset to next row')
+            print("Pixel on row {} and col {}" .format(i,j))
+            wpose = rc.move_group.get_current_pose().pose
+            waypoints = nextRow(wpose)
 
-    '''# reason for nested for loop:
-    Since we're using RGB image, that has 3 values per pixel instead of 1 in binary. We need to call individual pixel values (r,g,b) per pixel location in the RGB.
-    I treated the RGB as a matrix and have to call each pixel as [row,col] to get it. If I used .item() with an RGB, I'd get an indiviaul pixel value. 
-    For example:
-    the first pixel values for the 3x3 green_cross RGB is  red: 255 green: 0 blue: 0
-    Using item.() = > rgb_image.item(0)= 255 rgb_image.item(1)=0 rgb_image.item(2)= 0
-    You get each individual pixel, but since the image is a 3x3, there are 9 total pixels with 3 values in each which means the loop would have to run 27 times. If we use larger images, say 10x10, then the loop would run 300 times. 
-    
-    A nested for loop seems to be faster because you only need to run it the toal number of pixels (in this case of a 3x3, only 9 times)
-    row = (range(IMAGE_HEIGHT))
-    col = (range(IMAGE_WIDTH))
-    '''
-    
-    for i in row: #range(PIXEL_COUNT):
+        if move_robot:
+            plan, fraction = rc.plan_cartesian_path(waypoints)
+            rc.execute_plan(plan)
+
+            
         for j in col:
             print("Pixel on row {} and col {}" .format(i,j))
             wpose = rc.move_group.get_current_pose().pose
-            waypoints = []
+            r,g,b = img[i,j].astype('uint8')
 
-            waypoints.append(wpose)
-
-
-            r,g,b = rgb_img[i,j].astype('uint8')
-
-
-
-            if j == 0 and i == 0:
-                print('Initial Starting Position')
-                # print('row ',i)
-                # print('column',j)
-                waypoints = []
-                print('wpose.position.y=',wpose.position.y)
-
-            elif j % IMAGE_WIDTH == 0:
-                print('Moving to next row')
-                wpose.position.z -= MOTION_BOX_HEIGHT/IMAGE_HEIGHT
-                wpose.position.y = y_start # same y-axis starting value
-                waypoints.append(copy.deepcopy(wpose))
-                # print('row ',i)
-                # print('column',j)
-                # print('wpose.position.y=',wpose.position.y)
-
-            else: # keep incrementally moving horizontally across y-axis
+            if j != 0:
                 print('Keep moving horizontally')
-                # print('row ',i)
-                # print('column',j)
-                wpose.position.y += MOTION_BOX_WIDTH/IMAGE_WIDTH # Previously, MOTION_BOX_LENGTH/WIDTH = 3/9=1/3 m big jump
-                waypoints.append(copy.deepcopy(wpose))
-                    
+                waypoints = nextColumn(wpose)
+
             # input(f"Cartesian Plan {i}: press <enter>") # uncomment this line if you want robot to run automatically
             if move_robot:
                 plan, fraction = rc.plan_cartesian_path(waypoints)
@@ -135,32 +115,7 @@ def main():
                 sendRGB2LED(pub_rgb_values,r,g,b)
                 time.sleep(0.5)
                 sendRGB2LED(pub_rgb_values)
-                time.sleep(0.5)
-
-
-        # if i == 0: 
-        #     wpose.position.y = y_start # same y-axis starting value
-        #     waypoints = []
-        #     print('row ',i)
-        #     print('column',j)
-        #     # print('wpose.position.y=',wpose.position.y)
-
-        if i % IMAGE_WIDTH == 0: #
-            print('reset to next row')
-            print('row ',i)
-            print('column',j)
-
-            wpose.position.z -= MOTION_BOX_HEIGHT/IMAGE_HEIGHT
-            wpose.position.y = y_start # same y-axis starting value
-            waypoints.append(copy.deepcopy(wpose))
-            # print('wpose.position.y=',wpose.position.y)
-        else: 
-            wpose.position.y += MOTION_BOX_WIDTH/IMAGE_WIDTH 
-            waypoints.append(copy.deepcopy(wpose))
-            print('row ',i)
-            print('column',j)
-            # print('wpose.position.y=',wpose.position.y)
-        
+                time.sleep(0.5)        
     try:
         rospy.spin()
     except KeyboardInterrupt:
--- scripts/image_inputs.py
+++ scripts/image_inputs.py
@@ -26,12 +26,16 @@ binary = cv2.imread(join(BINARY,aims),0)
 green_cross = 'green_cross.tif' # 3x3 image
 blockO_rgb = 'block_o_RGB.tif' # 10x10 image
 
-# BGR = cv2.imread(join(R_G_B,green_cross))
-# RGB = cv2.cvtColor(BGR,cv2.COLOR_BGR2RGB)
-
-BGR = cv2.imread(join(R_G_B,blockO_rgb))
+BGR = cv2.imread(join(R_G_B,green_cross))
 RGB = cv2.cvtColor(BGR,cv2.COLOR_BGR2RGB)
 
+# BGR = cv2.imread(join(R_G_B,blockO_rgb))
+# RGB = cv2.cvtColor(BGR,cv2.COLOR_BGR2RGB)
+
+#GrayScale
+sweep_10x10 = 'sweep.tif'
+
+GS = cv2.imread(join(GRAYSCALE,sweep_10x10),0)
 
 # print('binary',binary)
 # Uncomment for debugging
