e55807ccac384d26980a08bd7d76166860f4c027
Mohammad Khan <mohammadukhan135@gmail.com>
2022-04-08 12:10:56 -0400

    Cleaned image input script

--- scripts/image_inputs.py
+++ scripts/image_inputs.py
@@ -18,19 +18,19 @@ white_rim4x3 = 'white_rim_4x3.tif'
 blockO = 'block-O_10x10.tif'
 aims = 'AIMS_20x5.tif'
 
-#RGB images:
-green_cross = 'green_cross.tif' # 3x3 image
-blockO_rgb = 'block_o_RGB.tif' # 10x10 image
-
 # binary = cv2.imread(join(BINARY,white_rim4x3),0)
 # binary = cv2.imread(join(BINARY,blockO),0)
 binary = cv2.imread(join(BINARY,aims),0)
 
-BGR = cv2.imread(join(R_G_B,green_cross))
-RGB = cv2.cvtColor(BGR,cv2.COLOR_BGR2RGB)
+#RGB images:
+green_cross = 'green_cross.tif' # 3x3 image
+blockO_rgb = 'block_o_RGB.tif' # 10x10 image
 
-# BGR = cv2.imread(join(R_G_B,blockO_rgb))
+# BGR = cv2.imread(join(R_G_B,green_cross))
 # RGB = cv2.cvtColor(BGR,cv2.COLOR_BGR2RGB)
+
+BGR = cv2.imread(join(R_G_B,blockO_rgb))
+RGB = cv2.cvtColor(BGR,cv2.COLOR_BGR2RGB)
 
 
 # print('binary',binary)
